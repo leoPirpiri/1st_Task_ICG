1d13b73345673bcbe1e41aaead73e00400dfb9a2
Live session user <leandrogomesdonascimento@gmail.com>
2018-08-09 23:29:41 +0000

    Primeiros Testes

--- cg_framework/definitions.h
+++ cg_framework/definitions.h
@@ -1,8 +1,8 @@
 #ifndef _DEFINITIONS_H_
 #define _DEFINITIONS_H_
 
-#define IMAGE_WIDTH  512 // número de colunas da imagem.
-#define IMAGE_HEIGHT 512 // número de linhas da imagem.
+#define IMAGE_WIDTH  960 // número de colunas da imagem.
+#define IMAGE_HEIGHT 540 // número de linhas da imagem.
 
 unsigned char * FBptr;
 
--- cg_framework/mygl.h
+++ cg_framework/mygl.h
@@ -7,8 +7,15 @@
 // Defina aqui as suas funções gráficas
 //*****************************************************************************
 
-
-
+	void print_a_dot(void) {
+	int middle_dot = 2*IMAGE_WIDTH*(IMAGE_HEIGHT+1);
+		for (int i=0; i<IMAGE_WIDTH*2; i++){
+			FBptr[middle_dot+i] = i/255; //componente R
+			FBptr[middle_dot+1+i] = i/255; //componente G
+			FBptr[middle_dot+2+i] = i/255; //componente B
+			FBptr[middle_dot+3+i] = 255; //componente A
+		}
+	}
 
 #endif // _MYGL_H_
 
