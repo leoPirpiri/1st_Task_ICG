71e6efa160b70e5c44adc3873eac9d6295cb6987
Leandro <leandrogomesdonascimento@gmail.com>
2018-08-10 09:48:10 -0300

    Testando linha de vida

--- cg_framework/mygl.h
+++ cg_framework/mygl.h
@@ -3,18 +3,37 @@
 
 #include "definitions.h"
 
+
 //*****************************************************************************
 // Defina aqui as suas funções gráficas
 //*****************************************************************************
 
-	void print_a_dot(void) {
+	int change_red = 255;
+	int change_blue = 255;
+	int change_green = 255;
+
+	void print_a_dot(int aux, int troca) {
+	aux=aux*4;
+	if (troca == 1){
+		change_red = rand()%255;
+		change_blue = rand()%255;
+		change_green = rand()%255;
+	}
 	int middle_dot = 2*IMAGE_WIDTH*(IMAGE_HEIGHT+1);
-		for (int i=0; i<IMAGE_WIDTH*2; i++){
-			FBptr[middle_dot+i] = i/255; //componente R
-			FBptr[middle_dot+1+i] = i/255; //componente G
-			FBptr[middle_dot+2+i] = i/255; //componente B
-			FBptr[middle_dot+3+i] = 255; //componente A
-		}
+		//for (int i=0; i<IMAGE_WIDTH*2; i+=4){
+			FBptr[middle_dot+aux] = change_red;   //Red
+			FBptr[middle_dot+1+aux] = change_green; //Green
+			FBptr[middle_dot+2+aux] = change_blue; //Blue
+			FBptr[middle_dot+3+aux] = 255; //Alpha
+			FBptr[(IMAGE_WIDTH*4)+middle_dot+aux] = change_red;   //Red
+			FBptr[(IMAGE_WIDTH*4)+middle_dot+1+aux] = change_green; //Green
+			FBptr[(IMAGE_WIDTH*4)+middle_dot+2+aux] = change_blue; //Blue
+			FBptr[(IMAGE_WIDTH*4)+middle_dot+3+aux] = 255; //Alpha
+			FBptr[(IMAGE_WIDTH*8)+middle_dot+aux] = change_red;   //Red
+			FBptr[(IMAGE_WIDTH*8)+middle_dot+1+aux] = change_green; //Green
+			FBptr[(IMAGE_WIDTH*8)+middle_dot+2+aux] = change_blue; //Blue
+			FBptr[(IMAGE_WIDTH*8)+middle_dot+3+aux] = 255; //Alpha
+		//}
 	}
 
 #endif // _MYGL_H_
